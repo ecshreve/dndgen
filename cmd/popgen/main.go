325c8c6f227cb830fabbd054c1391a90881680fb
shreve <eric@shreve.dev>
2024-09-19 00:06:52 +0000

    feat: new populator generate flow

---
+++ cmd/popgen/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"os"
+	"text/template"
+
+	"github.com/charmbracelet/log"
+
+	"github.com/samsarahq/go/oops"
+)
+
+func main() {
+	log.Info("Starting dndgen/popgen...")
+
+	tmplFile := "template/populator.tmpl"
+	tmpl := template.Must(template.ParseFiles(tmplFile))
+
+	var typeNamesToGenerate = []string{
+		"AbilityScore",
+		"Skill",
+		// "Language",
+		// "DamageType",
+		// "Race",
+		// "Class",
+		// "WeaponProperty",
+		// "MagicSchool",
+		// "RuleSection",
+		// "Rule",
+		// "Subrace",
+		// "Trait",
+		// "Coin",
+	}
+
+	ofile, err := os.Create("internal/popper/generated.go")
+	if err != nil {
+		log.Fatal(oops.Wrapf(err, "unable to create output file for populators"))
+	}
+	defer ofile.Close()
+
+	if err := tmpl.Execute(ofile, typeNamesToGenerate); err != nil {
+		log.Fatal(oops.Wrapf(err, "unable to execute template"))
+	}
+
+	log.Info("done")
+}
