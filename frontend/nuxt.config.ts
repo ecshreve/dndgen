2774e7c0be3cb042945109d78c78b8a6da746bb5
shreve <eric@shreve.dev>
2024-09-26 04:36:17 +0000

    feat: initialize template

---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,47 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  extends: ['@nuxt/ui-pro'],
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/ui',
+    '@vueuse/nuxt'
+  ],
+
+  ui: {
+    safelistColors: ['primary', 'red', 'orange', 'green']
+  },
+
+  colorMode: {
+    disableTransition: true
+  },
+
+  routeRules: {
+    // Temporary workaround for prerender regression. see https://github.com/nuxt/nuxt/issues/27490
+    '/': { prerender: true }
+  },
+
+  devtools: {
+    enabled: true
+  },
+
+  typescript: {
+    strict: false
+  },
+
+  future: {
+    compatibilityVersion: 4
+  },
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  },
+
+  compatibilityDate: '2024-07-11'
+})
