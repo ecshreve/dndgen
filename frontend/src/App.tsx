09b94cd6fbc565c0b292061a0d30eb532dc3771a
shreve <eric@shreve.dev>
2024-09-26 04:33:59 -0700

    feat: basic bio page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 import Link from '@mui/material/Link';
-import ProTip from './ProTip';
+import Typography from '@mui/material/Typography';
+import * as React from 'react';
+import CharacterBioPage from './CharacterBio';
 
 function Copyright() {
   return (
@@ -27,11 +27,7 @@ export default function App() {
   return (
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
-          Material UI Create React App example in TypeScript
-        </Typography>
-        <ProTip />
-        <Copyright />
+        <CharacterBioPage />
       </Box>
     </Container>
   );
