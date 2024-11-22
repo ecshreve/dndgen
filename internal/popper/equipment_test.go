d007358015bb596707d614885a64cfa3ad3d5906
shreve <eric@shreve.dev>
2024-11-22 11:32:39 -0800

    fix: cleanup tests for ci

--- internal/popper/equipment_test.go
+++ internal/popper/equipment_test.go
@@ -1,8 +1,11 @@
 package popper_test
 
 import (
+	"context"
 	"testing"
 
+	"entgo.io/ent/dialect/sql/schema"
+	"github.com/ecshreve/dndgen/ent"
 	"github.com/ecshreve/dndgen/internal/popper"
 	"github.com/ecshreve/dndgen/internal/utils"
 	"github.com/gkampitakis/go-snaps/snaps"
@@ -12,9 +15,39 @@ import (
 func TestEquipmentPopulator(t *testing.T) {
 	var data []popper.EquipmentJSON
 
-	err := utils.LoadJSONFile("data/Equipment.json", &data)
-	// err := utils.LoadJSONFile("testdata/multiEquipment.json", &data)
+	err := utils.LoadJSONFile("testdata/singleEquipment.json", &data)
 	require.NoError(t, err)
 
 	snaps.MatchJSON(t, data)
+
+	var multiData []popper.EquipmentJSON
+	err = utils.LoadJSONFile("testdata/multiEquipment.json", &multiData)
+	require.NoError(t, err)
+
+	snaps.MatchJSON(t, multiData)
+}
+
+func TestEquipmentPopulatorFull(t *testing.T) {
+	ctx := context.Background()
+	cl, err := ent.Open("sqlite3", "file:ent?mode=memory&_fk=1")
+	require.NoError(t, err)
+	defer cl.Close()
+
+	err = cl.Schema.Create(ctx, schema.WithGlobalUniqueID(true))
+	require.NoError(t, err)
+
+	pp := popper.NewPopper(ctx, cl, "../../data")
+	err = pp.PopulateAll(ctx)
+	require.NoError(t, err)
+
+	equipPop := popper.NewEquipmentPopulator(pp)
+	err = equipPop.Populate(ctx)
+	require.NoError(t, err)
+
+	allEquipment, err := cl.Equipment.Query().
+		WithCost().
+		All(ctx)
+	require.NoError(t, err)
+
+	snaps.MatchJSON(t, allEquipment)
 }
--- internal/popper/race_test.go
+++ internal/popper/race_test.go
@@ -14,11 +14,6 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-type testRace struct {
-	Indx string `json:"index"`
-	Name string `json:"name"`
-}
-
 func TestRacePopulator(t *testing.T) {
 	var data []popper.RaceJSON
 
@@ -30,18 +25,18 @@ func TestRacePopulator(t *testing.T) {
 
 func TestRacePopulatorFull(t *testing.T) {
 	ctx := context.Background()
-	// cl, err := ent.Open("sqlite3", "file:ent?cache=shared&mode=memory&_fk=1")
-	cl, err := ent.Open("sqlite3", "file:testdb.db?_fk=1")
+	cl, err := ent.Open("sqlite3", "file:ent?mode=memory&_fk=1")
 	require.NoError(t, err)
+	defer cl.Close()
 
 	err = cl.Schema.Create(ctx, schema.WithGlobalUniqueID(true))
 	require.NoError(t, err)
 
-	pp := popper.NewPopper(ctx, cl, "data")
+	pp := popper.NewPopper(ctx, cl, "../../data")
 	err = pp.PopulateAll(ctx)
 	require.NoError(t, err)
 
-	profPop := popper.NewProficiencyPopulator(pp, "data/Proficiency.json")
+	profPop := popper.NewProficiencyPopulator(pp, "../../data/Proficiency.json")
 	err = profPop.Populate(ctx)
 	require.NoError(t, err)
 
